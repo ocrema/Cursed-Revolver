2e84103e091bd8eaed02f4c804b1993623a3436e
Bindi <bindin42@gmail.com>
2025-02-01 21:54:19 -0800

    Undoing main calls for cactus

--- Entities/Map1.js
+++ Entities/Map1.js
@@ -15,15 +15,5 @@ export class Map1 extends GameMap {
         GAME_ENGINE.addEntity(new Platform(300, 200, 200, 400));
         GAME_ENGINE.addEntity(new Platform(1200, 200, 200, 400));
         GAME_ENGINE.addEntity(new Platform(-600, 0, 600, 100));
-
-        const cactus1 = new Cactus();
-        cactus1.x = 600;
-        cactus1.y = -300;
-        const cactus2 = new Cactus();
-        cactus2.x = 1500;
-        cactus2.y = -100;
-        GAME_ENGINE.addEntity(cactus1);
-        GAME_ENGINE.addEntity(cactus2);
-
     }
 }
