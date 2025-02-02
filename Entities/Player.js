b75ea86866e79a107ff592f04f7391b42d0344de
ocrema <166955526+ocrema@users.noreply.github.com>
2025-02-02 14:56:02 -0800

    added blast jump

--- Entities/Player.js
+++ Entities/Player.js
@@ -94,6 +94,8 @@ export class Player extends Actor {
     this.speed = 500; // Movement speed
     this.isMoving = false; // Whether the player is moving
     this.health = 200;
+    this.isLaunchable = true;
+    this.validEffects = {};
 
     // Start with the idle animation
     this.setAnimation(PLAYER_SPRITESHEET.IDLE.NAME);
@@ -117,29 +119,9 @@ export class Player extends Actor {
   jump() { }
 
   update() {
-    //console.log(this.colliders);
-    //super.applyGravity(1);
+
     this.isMoving = false;
     this.isJumping = false;
-    //console.log(this.x + " " + this.y);
-
-    this.y_velocity = Math.min(
-      this.y_velocity + GAME_ENGINE.clockTick * 3000,
-      10000
-    );
-    this.x_velocity = 0;
-
-    // Movement logic
-    if (GAME_ENGINE.keys["a"]) {
-      this.x_velocity -= this.speed;
-      this.isMoving = true;
-      this.flip = true;
-    }
-    if (GAME_ENGINE.keys["d"]) {
-      this.x_velocity += this.speed;
-      this.isMoving = true;
-      this.flip = false;
-    }
 
     // Player Reset Button - this is if the player dies, this resets player health and respawns them.
     if (GAME_ENGINE.keys["h"]) {
@@ -148,6 +130,153 @@ export class Player extends Actor {
       this.x = 0;
       this.y = 0;
       this.setAnimation(PLAYER_SPRITESHEET.IDLE.NAME);
+    }
+
+    this.movement();
+
+    this.spells();
+
+
+
+    // footstep sfx
+    if (this.isMoving && this.isGrounded) {
+      this.timeSinceLastFootstep += GAME_ENGINE.clockTick;
+      if (this.timeSinceLastFootstep >= this.timeBetweenFootsteps) {
+        this.timeSinceLastFootstep = 0;
+        window.ASSET_MANAGER.playAsset("./assets/sfx/footstep.wav");
+      }
+    }
+
+    // Player State Logic
+    if (!this.isDead) {
+      if (this.hitTimer > 0) {
+        this.hitTimer -= GAME_ENGINE.clockTick;
+      } else if (
+        this.currentAnimation === PLAYER_SPRITESHEET.ATTACK1.NAME ||
+        this.currentAnimation === PLAYER_SPRITESHEET.ATTACK2.NAME
+      ) {
+        // Do nothing, let the attack animation play out
+      } else {
+        // Only switch animations if the hit animation is NOT playing
+        if (!this.isGrounded) {
+          if (this.y_velocity < 0) {
+            this.setAnimation(PLAYER_SPRITESHEET.JUMP.NAME);
+          } else {
+            this.setAnimation(PLAYER_SPRITESHEET.FALL.NAME);
+          }
+        } else if (this.isMoving) {
+          this.setAnimation(PLAYER_SPRITESHEET.RUN.NAME);
+        } else {
+          this.setAnimation(PLAYER_SPRITESHEET.IDLE.NAME);
+        }
+      }
+    } else {
+      this.setAnimation(PLAYER_SPRITESHEET.DEAD.NAME, false);
+    }
+
+    if (
+      this.recieved_attacks.length > 0 &&
+      this.currentAnimation !== PLAYER_SPRITESHEET.HIT.NAME
+    ) {
+      console.log(
+        "ouch! i took " + this.recieved_attacks[0].damage + " damage"
+      );
+
+      this.setAnimation(PLAYER_SPRITESHEET.HIT.NAME);
+      //this.health -= this.recieved_attacks[0].damage;
+
+      this.hitTimer = 0.3;
+    }
+
+    this.recieveAttacks();
+    this.recieveEffects();
+
+
+    if (this.health <= 0) {
+      this.isDead = true;
+      this.setAnimation(PLAYER_SPRITESHEET.DEAD.NAME, false);
+      console.log("i died");
+    }
+
+    // Update the active animation
+    this.updateAnimation(GAME_ENGINE.clockTick);
+  }
+
+  onAnimationComplete() {
+    // Check if the current animation is the attack animation
+    if (
+      this.currentAnimation === PLAYER_SPRITESHEET.ATTACK1.NAME ||
+      this.currentAnimation === PLAYER_SPRITESHEET.ATTACK2.NAME
+    ) {
+      // Switch back to the idle animation after the attack animation completes
+      this.setAnimation(PLAYER_SPRITESHEET.IDLE.NAME);
+    }
+  }
+
+  spells() {
+    // Player Attack Logic
+
+    for (let i = 0; i < this.spellCooldowns.length; i++) {
+      this.spellCooldowns[i] = Math.max(
+        this.spellCooldowns[i] - GAME_ENGINE.clockTick,
+        0
+      );
+      const key = (i + 1).toString();
+      if (GAME_ENGINE.keys[key]) {
+        GAME_ENGINE.keys[key] = false;
+        this.selectedSpell = i;
+        window.ASSET_MANAGER.playAsset("./assets/sfx/click1.ogg");
+      }
+    }
+
+    // cast spell
+
+    if (this.spellCooldowns[this.selectedSpell] <= 0 && GAME_ENGINE.keys["m1"]) {
+      this.spellCooldowns[this.selectedSpell] = this.maxSpellCooldown;
+      window.ASSET_MANAGER.playAsset("./assets/sfx/revolver_shot.ogg", 1);
+
+      if (this.selectedSpell === 0) {
+        const fireball = new Fireball();
+        fireball.x = this.x;
+        fireball.y = this.y;
+        fireball.dir = Util.getAngle(
+          {
+            x: this.x - GAME_ENGINE.camera.x,
+            y: this.y - GAME_ENGINE.camera.y
+          },
+          {
+            x: GAME_ENGINE.mouse.x,
+            y: GAME_ENGINE.mouse.y
+          }
+        );
+        GAME_ENGINE.addEntity(fireball);
+      }
+      else if (this.selectedSpell === 1) {
+        const chain_lightning = new ChainLightning(this, Util.getAngle(
+          {
+            x: this.x - GAME_ENGINE.camera.x,
+            y: this.y - GAME_ENGINE.camera.y
+          },
+          {
+            x: GAME_ENGINE.mouse.x,
+            y: GAME_ENGINE.mouse.y
+          }));
+        GAME_ENGINE.addEntity(chain_lightning);
+      }
+
+    }
+  }
+
+  movement() {
+    this.y_velocity = Math.min(
+      this.y_velocity + GAME_ENGINE.clockTick * 3000,
+      10000
+    );
+    if (this.x_velocity > 0) {
+      this.x_velocity = Math.max(this.x_velocity - GAME_ENGINE.clockTick * (this.isGrounded == .2 ? 9000 : 1000), 0);
+    }
+    else {
+      this.x_velocity = Math.min(this.x_velocity + GAME_ENGINE.clockTick * (this.isGrounded == .2 ? 9000 : 1000), 0);
     }
 
     this.isGrounded = Math.max(this.isGrounded - GAME_ENGINE.clockTick, 0);
@@ -160,9 +289,23 @@ export class Player extends Actor {
       window.ASSET_MANAGER.playAsset("./assets/sfx/jump.ogg");
     }
 
+    // Movement logic
+    
+    let velFromKeys = 0
+    if (GAME_ENGINE.keys["a"]) {
+      velFromKeys -= this.speed;
+      this.isMoving = true;
+      this.flip = true;
+    }
+    if (GAME_ENGINE.keys["d"]) {
+      velFromKeys += this.speed;
+      this.isMoving = true;
+      this.flip = false;
+    }
+
     // Player Collision Logic
 
-    this.x += this.x_velocity * GAME_ENGINE.clockTick;
+    this.x += (this.x_velocity + velFromKeys) * GAME_ENGINE.clockTick;
     let collisions = [];
     for (let e of GAME_ENGINE.entities) {
       if (e.isPlayer || e.isAttack) continue;
@@ -171,7 +314,7 @@ export class Player extends Actor {
       }
     }
     if (collisions.length !== 0) {
-      if (this.x_velocity > 0) {
+      if (this.x_velocity + velFromKeys > 0) {
         this.x =
           collisions.reduce(
             (acc, curr) => Math.min(acc, curr.x - curr.collider.width / 2),
@@ -222,139 +365,6 @@ export class Player extends Actor {
 
 
       this.y_velocity = 0;
-    }
-
-    // Player Attack Logic
-
-    for (let i = 0; i < this.spellCooldowns.length; i++) {
-      this.spellCooldowns[i] = Math.max(
-        this.spellCooldowns[i] - GAME_ENGINE.clockTick,
-        0
-      );
-      const key = (i + 1).toString();
-      if (GAME_ENGINE.keys[key]) {
-        GAME_ENGINE.keys[key] = false;
-        this.selectedSpell = i;
-        window.ASSET_MANAGER.playAsset("./assets/sfx/click1.ogg");
-      }
-    }
-
-    // cast spell
-
-    if (this.spellCooldowns[this.selectedSpell] <= 0 && GAME_ENGINE.keys["m1"]) {
-      this.spellCooldowns[this.selectedSpell] = this.maxSpellCooldown;
-      window.ASSET_MANAGER.playAsset("./assets/sfx/revolver_shot.ogg", 1);
-
-      if (this.selectedSpell === 0) {
-        const fireball = new Fireball();
-        fireball.x = this.x;
-        fireball.y = this.y;
-        fireball.dir = Util.getAngle(
-          {
-            x: this.x - GAME_ENGINE.camera.x,
-            y: this.y - GAME_ENGINE.camera.y
-          },
-          {
-            x: GAME_ENGINE.mouse.x,
-            y: GAME_ENGINE.mouse.y
-          }
-        );
-        GAME_ENGINE.addEntity(fireball);
-      }
-      else if (this.selectedSpell === 1) {
-        const chain_lightning = new ChainLightning(this, Util.getAngle(
-          {
-            x: this.x - GAME_ENGINE.camera.x,
-            y: this.y - GAME_ENGINE.camera.y
-          },
-          {
-            x: GAME_ENGINE.mouse.x,
-            y: GAME_ENGINE.mouse.y
-          }));
-          GAME_ENGINE.addEntity(chain_lightning);
-      }
-
-    }
-
-    // footstep sfx
-    if (this.isMoving && this.isGrounded) {
-      this.timeSinceLastFootstep += GAME_ENGINE.clockTick;
-      if (this.timeSinceLastFootstep >= this.timeBetweenFootsteps) {
-        this.timeSinceLastFootstep = 0;
-        window.ASSET_MANAGER.playAsset("./assets/sfx/footstep.wav");
-      }
-    }
-
-    // Player State Logic
-    if (!this.isDead) {
-      if (this.hitTimer > 0) {
-        this.hitTimer -= GAME_ENGINE.clockTick;
-      } else if (
-        this.currentAnimation === PLAYER_SPRITESHEET.ATTACK1.NAME ||
-        this.currentAnimation === PLAYER_SPRITESHEET.ATTACK2.NAME
-      ) {
-        // Do nothing, let the attack animation play out
-      } else {
-        // Only switch animations if the hit animation is NOT playing
-        if (!this.isGrounded) {
-          if (this.y_velocity < 0) {
-            this.setAnimation(PLAYER_SPRITESHEET.JUMP.NAME);
-          } else {
-            this.setAnimation(PLAYER_SPRITESHEET.FALL.NAME);
-          }
-        } else if (this.isMoving) {
-          this.setAnimation(PLAYER_SPRITESHEET.RUN.NAME);
-        } else {
-          this.setAnimation(PLAYER_SPRITESHEET.IDLE.NAME);
-        }
-      }
-    } else {
-      this.setAnimation(PLAYER_SPRITESHEET.DEAD.NAME, false);
-    }
-
-    if (
-      this.recieved_attacks.length > 0 &&
-      this.currentAnimation !== PLAYER_SPRITESHEET.HIT.NAME
-    ) {
-      console.log(
-        "ouch! i took " + this.recieved_attacks[0].damage + " damage"
-      );
-
-      this.setAnimation(PLAYER_SPRITESHEET.HIT.NAME);
-      this.health -= this.recieved_attacks[0].damage;
-
-      this.hitTimer = 0.3;
-    }
-
-    this.recieved_attacks = [];
-
-    // process each attack
-
-    // this.recieved_attacks.forEach((attack) => {
-    //   console.log("ouch! i took " + attack.damage + " damage");
-    //   this.setAnimation(PLAYER_SPRITESHEET.HIT.NAME);
-    //   this.health -= attack.damage;
-    // });
-    // this.recieved_attacks = [];
-
-    if (this.health <= 0) {
-      this.isDead = true;
-      this.setAnimation(PLAYER_SPRITESHEET.DEAD.NAME, false);
-      console.log("i died");
-    }
-
-    // Update the active animation
-    this.updateAnimation(GAME_ENGINE.clockTick);
-  }
-
-  onAnimationComplete() {
-    // Check if the current animation is the attack animation
-    if (
-      this.currentAnimation === PLAYER_SPRITESHEET.ATTACK1.NAME ||
-      this.currentAnimation === PLAYER_SPRITESHEET.ATTACK2.NAME
-    ) {
-      // Switch back to the idle animation after the attack animation completes
-      this.setAnimation(PLAYER_SPRITESHEET.IDLE.NAME);
     }
   }
 }
