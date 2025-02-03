2f478c06d7ab2a6e759f6fea5bf2963b003be7a3
Ares <areszee7@gmail.com>
2025-02-02 20:00:55 -0800

    Reorganized asset downloading in AssetDownloader

--- Entities/Map1.js
+++ Entities/Map1.js
@@ -15,32 +15,8 @@ export class Map1 extends GameMap {
     GAME_ENGINE.addEntity(new Cactus(-750, 380));
     GAME_ENGINE.addEntity(new Spider(800, 370));
     GAME_ENGINE.addEntity(new Background());
-    // Platform Parameters - X, Y, Width, Height
-
-    // Main Floor (long ground platform)
-
-    // const mainPlatform = new Structure(0, 800);
-    // mainPlatform.addPlatform(0, 0, 100, 10); // Main floor
-    // mainPlatform.addPlatform(5, -200, 1, 12); // Left Pillar
     GAME_ENGINE.addEntity(new Platform(0, 800, 100, 10));
     GAME_ENGINE.addEntity(new Platform(150, 400, 1, 6));
     GAME_ENGINE.addEntity(new Platform(-300, 20, 10, 1));
-    //mainPlatform.addPlatform(50, -200, 1, 12); // Right Pillar
-
-    // // Floating Platforms (Jumping platforms)
-    // const horizontalFloatingPlatforms = new Structure(-200, 0);
-
-    // // Parameters - x, y, width, height, scaleX, scaleY
-    // horizontalFloatingPlatforms.addPlatform(-400, 100, 2, 80); // Left floating platform
-    // horizontalFloatingPlatforms.addPlatform(100, 50, 2, 80); // Right floating platform
-    // horizontalFloatingPlatforms.addPlatform(-100, -50, 3, 80); // Center higher platform
-    // horizontalFloatingPlatforms.scale(2, 1);
-
-    // Vertical Pillars
-    //const verticalPillars = new Structure(1200, 400);
-
-    // verticalPillars.addPlatform(300, -200, 1, 300); // Left Pillar
-    // verticalPillars.addPlatform(600, -200, 1, 300); // Right Pillar
-    // verticalPillars.scale(1.5,1.1);
   }
 }
--- main.js
+++ main.js
@@ -13,51 +13,8 @@ const ASSET_DOWNLOADER = new AssetDownloader(ASSET_MANAGER);
 // Loading Sprite Sheets
 
 ASSET_DOWNLOADER.downloadAll();
-// Player
-// ASSET_MANAGER.queueDownload("./assets/player/Idle.png");
-// ASSET_MANAGER.queueDownload("./assets/player/Run.png");
-// ASSET_MANAGER.queueDownload("./assets/player/Jump.png");
-// ASSET_MANAGER.queueDownload("./assets/player/Death.png");
-// ASSET_MANAGER.queueDownload("./assets/player/Fall.png");
 
-// // Enemy
-// ASSET_MANAGER.queueDownload("./assets/cactus/cactus.png");
 
-// // Enemy Attack
-// ASSET_MANAGER.queueDownload("./assets/thorn/thorn.png");
-
-// // Background
-// ASSET_MANAGER.queueDownload("./assets/background/background.png");
-
-// Pause Menu Buttons
-ASSET_MANAGER.queueDownload("./assets/ui/menu/buttonResume.png");
-ASSET_MANAGER.queueDownload("./assets/ui/menu/buttonSettings.png");
-ASSET_MANAGER.queueDownload("./assets/ui/menu/buttonQuit.png");
-ASSET_MANAGER.queueDownload("./assets/ui/menu/buttonStart.png");
-ASSET_MANAGER.queueDownload("./assets/ui/menu/menuBackground.png");
-ASSET_MANAGER.queueDownload("./assets/ui/menu/background_start.png");
-ASSET_MANAGER.queueDownload("./assets/ui/menu/background_start2.png");
-ASSET_MANAGER.queueDownload("./assets/ui/menu/desert.jpg");
-
-//spells for hud
-ASSET_MANAGER.queueDownload("./assets/ui/spells/fireball.gif");
-ASSET_MANAGER.queueDownload("./assets/ui/spells/lightning.gif");
-ASSET_MANAGER.queueDownload("./assets/ui/spells/water.gif");
-ASSET_MANAGER.queueDownload("./assets/ui/spells/icicle.gif");
-ASSET_MANAGER.queueDownload("./assets/ui/spells/vine.gif");
-ASSET_MANAGER.queueDownload("./assets/ui/spells/void.gif");
-
-//cowboy
-ASSET_MANAGER.queueDownload("./assets/ui/cowboy.png");
-
-//healthbar
-ASSET_MANAGER.queueDownload("./assets/ui/cowboy.png");
-ASSET_MANAGER.queueDownload("./assets/ui/healthbar.png");
-
-// Loading Revolver Cylinder Sprites (Cylinder 1 to 10)
-for (let i = 1; i <= 10; i++) {
-  ASSET_MANAGER.queueDownload(`./assets/ui/revolver/cylinder${i}.png`);
-}
 
 ASSET_MANAGER.downloadAll(() => {
   const canvas = document.getElementById("gameWorld");
