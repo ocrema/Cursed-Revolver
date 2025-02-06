532f64ca760c8f24800c24ed92d8c85bbba18974
Ares <areszee7@gmail.com>
2025-02-06 14:11:55 -0800

    updated spider to recieve effects

--- Entities/Enemy/Enemy.js
+++ Entities/Enemy/Enemy.js
@@ -137,29 +137,130 @@ export class Spider extends Actor {
   }
 
   update() {
+    this.recieveEffects()
     this.seesPlayer = false;
     // if currently attacking, don't update attack cooldown
-    this.attackCooldown += this.currentAnimation === "attack" ? 0 : GAME_ENGINE.clockTick;
-    this.attackCooldown += GAME_ENGINE.clockTick;
-    this.recieveEffects();
-    this.onGround = false;
-    this.onWall = false;
+    this.attackCooldown +=
+      this.currentAnimation === "attack" ? 0 : GAME_ENGINE.clockTick;
 
     // update target
     for (let entity of GAME_ENGINE.entities) {
       if (entity instanceof Player && Util.canSee(this, entity)) {
-        this.target = { x: entity.x, y: entity.y };
         if (
-          this.currentAnimation === "walk" ||
-          this.currentAnimation === "idle"
+          this.currentAnimation === "aggressive" ||
+          this.currentAnimation === "attack"
         ) {
-          this.setAnimation("aggresive");
+          this.target = { x: entity.x, y: entity.y };
         }
       }
     }
 
+    // updates what state the spider is in and moves target when state changes
+    this.setState();
+
+    // moves to target, deals with platform collision
+    this.movement();
+
+    // flip image according to velocity
+    if (this.velocity.x < 0) {
+      this.flip = 0;
+    } else if (this.velocity.x > 0) {
+      this.flip = 1;
+    }
+
+    // apply attack damage
+    for (let attack of this.recieved_attacks) {
+      this.health -= attack.damage;
+    }
+    this.recieved_attacks = [];
+
+    // if spider loses all health
+    if (this.health <= 0) {
+      this.removeFromWorld = true;
+    }
+
+    // debug statement, delete this eventually ^_^
+    if (this.y > 490) {
+      console.log(this);
+    }
+  }
+
+  // cycles through different cases to set animation state
+  setState() {
+    // if cant see player and close to target location
+    if (!this.seesPlayer && Math.abs(this.x - this.target.x) < 5) {
+      // change animation and speed
+      this.setAnimation("roam");
+      this.speed = this.walkSpeed;
+
+      if (this.velocity.x < 0) {
+        // approaching target from the left
+        this.target.x +=
+          this.randomRoamLength[Util.randomInt(this.randomRoamLength.length)]; // run to the right
+      } else {
+        // approaching target from the right
+        this.target.x -=
+          this.randomRoamLength[Util.randomInt(this.randomRoamLength.length)]; // run to the left
+      }
+    }
+
+    if (this.seesPlayer && this.attackCooldown < this.attackRate) {
+      this.setAnimation("run");
+    }
+
+    // if can see player and can attack
+    if (
+      this.seesPlayer &&
+      this.attackCooldown > this.attackRate &&
+      this.currentAnimation !== "aggressive"
+    ) {
+      this.setAnimation("aggressive");
+      this.speed = this.aggroSpeed;
+    }
+
+    // if can see player, can attack, and is in attack radius
+    if (
+      this.seesPlayer &&
+      this.attackCooldown > this.attackRate &&
+      Math.abs(this.x - this.target.x) < this.attackRadius
+    ) {
+      // change animation and speed
+      this.setAnimation("attack");
+      this.speed = this.attackSpeed;
+
+      // if no jaw, spawn one in + reset the timer
+      if (!this.jaw) {
+        this.attackCooldown = 0;
+        this.jaw = new Jaw(this);
+        GAME_ENGINE.addEntity(this.jaw);
+      }
+    }
+
+    if (
+      this.currentAnimation === "run" &&
+      Math.abs(this.x - this.target.x) < 5
+    ) {
+      this.speed = this.runSpeed;
+      if (this.velocity.x < 0) {
+        // approaching target from the left
+        this.target.x +=
+          this.randomRunLength[Util.randomInt(this.randomRunLength.length)]; // run to the right
+      } else {
+        // approaching target from the right
+        this.target.x -=
+          this.randomRunLength[Util.randomInt(this.randomRunLength.length)]; // run to the left
+      }
+    }
+  }
+
+  movement() {
+    // update location
+    this.x += this.velocity.x * GAME_ENGINE.clockTick;
+    this.y += this.velocity.y * GAME_ENGINE.clockTick;
+
     // update velocity
     var distance = Util.getDistance(this, this.target);
+
     this.velocity = {
       x: ((this.target.x - this.x) / distance) * this.runSpeed,
       y: 0,
@@ -172,7 +273,6 @@ export class Spider extends Actor {
         entity.collider &&
         this.colliding(entity)
       ) {
-        //console.log("spider colligind");
         let thisTop = this.y - this.height / 2;
         let thisBottom = this.y + this.height / 2;
         let thisLeft = this.x - this.width / 2;
@@ -190,35 +290,33 @@ export class Spider extends Actor {
           thisBottom > eTop &&
           thisTop < eTop &&
           thisRight > eLeft &&
-          thisLeft < eRight; // true if top of spider and platform match
+          thisLeft < eRight;
         let collideTop =
           thisTop > eBottom &&
           thisBottom < eBottom &&
           thisRight > eLeft &&
           thisLeft < eRight;
 
-        if (collideBottom) {
+        if (collideBottom && this.velocity.y > 0) {
+          // if colliding ground and moving down
           this.y = entity.y - entity.collider.height / 2 - this.height / 2;
           this.onGround = true;
         }
 
-        if (
-          !this.onGround &&
-          ((collideRight && this.velocity.x > 0) || // if colliding wall on right and moving right
-            (collideLeft && this.velocity.x < 0))
-        ) {
-          // if colliding wall on left and moving left
+        if (collideLeft || collideRight) {
+          this.target.y = eTop - this.height / 2;
+          this.velocity.y =
+            ((this.target.y - this.y) / distance) *
+            this.runSpeed *
+            this.climbSpeed;
           this.velocity.x = 0;
-          this.target.y = eTop - this.height;
-          this.onWall = true;
-        }
-
-        if (
-          Math.abs(
-            this.y - (entity.y - entity.collider.height / 2 - this.height / 2)
-          ) < 5
-        ) {
-          this.onGround = true;
+          if (collideRight && this.velocity.x > 0) {
+            // if colliding with wall on right and moving right
+            this.x = entity.x - entity.collider.width / 2 - this.width / 2;
+          } else if (collideLeft && this.velocity.x < 0) {
+            // if colliding with wall on left and moving left
+            this.x = entity.x + entity.collider.width / 2 + this.width / 2;
+          }
         }
       }
     }
