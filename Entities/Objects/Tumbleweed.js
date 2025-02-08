72e60e484804f244b42488ed2d152f7d1d9b1f14
Ares <areszee7@gmail.com>
2025-02-08 00:13:27 -0800

    adjusted tumbleweed speed

--- Entities/Objects/Tumbleweed.js
+++ Entities/Objects/Tumbleweed.js
@@ -12,7 +12,7 @@ export class Tumbleweed extends Entity {
     this.entityOrder = 2; // Render below spells
     this.assetManager = window.ASSET_MANAGER;
     this.direction = direction;
-    this.speed = 200; // Horizontal movement speed
+    this.speed = 250; // Horizontal movement speed
     this.rotation = 0; // Rotation angle
     this.rotationSpeed = 0.1; // Speed of rolling rotation
     this.scale = 0.35; // Scale the sprite
