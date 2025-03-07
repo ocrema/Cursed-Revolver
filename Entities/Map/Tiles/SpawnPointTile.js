ea94b5cf7f4563d1d4d93b43b7a27bd0bae75149
Ares <areszee7@gmail.com>
2025-03-06 23:23:27 -0800

    not working enemy respawn ssytem for checkpoints

--- Entities/Map/Tiles/SpawnPointTile.js
+++ Entities/Map/Tiles/SpawnPointTile.js
@@ -2,6 +2,7 @@ import { Collider } from "../../Collider.js";
 import { Tile } from "./Tile.js";
 import { Player } from "../../Player/Player.js";
 import { SpawnPoint } from "../../Objects/TileVisuals/SpawnPoint.js";
+import { GAME_ENGINE } from "../../../main.js";
 
 export class SpawnPointTile extends Tile {
   constructor(
@@ -27,11 +28,17 @@ export class SpawnPointTile extends Tile {
     this.scale = scale;
     this.isGround = true;
     this.isSpawnPoint = true;
+    this.respawnX = this.x; // Track respawn X position
+    this.killedEnemies = new Set(); // Track killed enemies
+    this.respawnedEnemies = new Set(); // Track enemies that have been respawned
+    this.isActivated = false;
 
     this.collider = new Collider(
       this.tileSize * this.scale,
       this.tileSize * this.scale
     );
+
+    this.map = window.MAP;
 
     // Spawn a SpawnPoint entity for the visual effect
     this.spawnPointEntity = new SpawnPoint(this.x, this.y, this.scale);
@@ -39,10 +46,55 @@ export class SpawnPointTile extends Tile {
   }
 
   update() {
+    // for (let e of GAME_ENGINE.entities) {
+    //   if (e instanceof Player && this.colliding(e) && GAME_ENGINE.keys["f"]) {
+    //     if (!this.isActivated) {
+    //       this.isActivated = true;
+    //       this.respawnEnemies();
+    //       console.log("Player reached new spawn point at:", this.x);
+    //       this.respawnX = this.x; // Update respawn position
+    //       e.setSpawnPoint(this.x, this.y - e.collider.height / 2 - 1);
+    //       this.respawnedEnemies.clear(); // Allow future respawns
+    //     } else {
+    //       this.isActivated = false;
+    //     }
+    //   }
+    // }
     for (let e of GAME_ENGINE.entities) {
       if (e instanceof Player && this.colliding(e)) {
         //console.log("Player hit checkpoint at " + e.x + " " + e.y);
         e.setSpawnPoint(this.x, this.y - e.collider.height / 2 - 1);
+      }
+    }
+  }
+
+  onEnemyDeath(enemy) {
+    let enemyKey = `${enemy.x}-${enemy.y}`;
+
+    if (enemy.x > this.respawnX) {
+      console.log(`Tracking enemy death at ${enemyKey}`);
+      this.killedEnemies.add(enemyKey);
+    }
+  }
+
+  respawnEnemies() {
+    if (!this.isActivated) return; // Only respawn enemies if the spawn point was activated
+
+    console.log("Respawning enemies ahead of checkpoint:", this.respawnX);
+
+    for (const stage in this.map.enemySpawnData) {
+      for (let spawn of this.map.enemySpawnData[stage]) {
+        let enemyKey = `${spawn.x}-${spawn.y}`;
+
+        if (spawn.x > this.respawnX && !this.respawnedEnemies.has(enemyKey)) {
+          console.log(`Respawning enemy at ${spawn.x}, ${spawn.y}`);
+
+          let enemy = new spawn.type(spawn.x, spawn.y);
+          enemy.onDeath = () => this.map.onEnemyDeath(enemy);
+          GAME_ENGINE.addEntity(enemy);
+
+          this.respawnedEnemies.add(enemyKey); // Mark this enemy as respawned
+        }
       }
     }
   }
