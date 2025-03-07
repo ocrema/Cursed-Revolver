09419eec418a487a4eb68944b00f3f150e68ab3c
Ares <areszee7@gmail.com>
2025-03-07 00:50:25 -0800

    added boulder and spiderweb obstacles

---
+++ Entities/Objects/Boulder.js
@@ -0,0 +1,50 @@
+import { Actor } from "../Actor.js";
+import { Collider } from "../Collider.js";
+import { GAME_ENGINE } from "../../main.js";
+
+export class Boulder extends Actor {
+  constructor(x, y) {
+    super();
+    Object.assign(this, { x, y });
+
+    this.spawnX = x;
+    this.spawnY = y;
+
+    // Load Image
+    this.image = window.ASSET_MANAGER.getAsset("./assets/objects/Boulder.png");
+
+    // Set dimensions
+    this.width = 24;
+    this.height = 24;
+
+    // Health
+    this.health = 50;
+    this.maxHealth = 50;
+
+    this.scale = 12;
+
+    // Collider
+    this.collider = new Collider(
+      this.width * this.scale - this.scale * 5,
+      this.height * this.scale - this.scale * 8
+    );
+  }
+
+  update() {
+    this.recieveAttacks();
+
+    if (this.health <= 0) {
+      this.removeFromWorld = true;
+    }
+  }
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.x - GAME_ENGINE.camera.x - (this.width * this.scale) / 2,
+      this.y - GAME_ENGINE.camera.y - (this.height * this.scale) / 2,
+      this.width * this.scale,
+      this.height * this.scale
+    );
+  }
+}
